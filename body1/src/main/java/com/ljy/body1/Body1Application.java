ff45779e92e391cf5dae091887cb430e92c528b1
lujunyu <lujunyu@vipkid.com.cn>
2019-05-22 15:09:06 +0800

    首次提交

---
+++ body1/src/main/java/com/ljy/body1/Body1Application.java
@@ -0,0 +1,13 @@
+package com.ljy.body1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Body1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Body1Application.class, args);
+    }
+
+}
